ed217cbcbf48b69d199d4848a6c1ad1f612c5a68
meap02 <79549780+meap02@users.noreply.github.com>
2023-11-14 19:53:16 -0800

    CLI Functional
    Using the cmd module I created an interactive shell to send emails using the ClientUtils module. Also added the Validators class for future implementation of input validation

--- Client/FEEmailClient.py
+++ Client/FEEmailClient.py
@@ -1,44 +1,51 @@
 
-import argparse
-from ClientUtils import *
+import cmd
+from ClientUtils import ClientUtils
+import json
+from getpass import getpass
 
-# Create the parser
-parser = argparse.ArgumentParser(description='FEE-Mail Client')
+class ClientShell(cmd.Cmd):
+    intro = 'Welcome to the FEE-Mail client shell. Type help or ? to list commands.\n'
+    prompt = '(FEE-Mail)$'
 
-# Add the arguments
-parser.add_argument('command', type=str, help='The command to execute')
-parser.add_argument('--username', type=str, help='The username for authentication')
-parser.add_argument('--password', type=str, help='The password for authentication')
-parser.add_argument('--smtp-server', type=str, help='The SMTP server to connect to')
-parser.add_argument('--smtp-port', type=int, help='The port to connect to the SMTP server on')
-parser.add_argument('--to', type=str, help='The recipient of the email')
-parser.add_argument('--subject', type=str, help='The subject of the email')
-parser.add_argument('--body', type=str, help='The body of the email')
+    def __init__(self):
+        super().__init__()
+        creds_exists = input('Do you have a creds.json file? (y/n) ')
+        if creds_exists == 'y':
+            creds_file = input('Enter creds.json file path: ')
+            with open(creds_file) as f:
+                creds = json.load(f)
+            name = input('Enter your name: ')
+            smtp_server = creds[name]['smtp_server']
+            smtp_port = creds[name]['smtp_port']
+            username = creds[name]['username']
+            password = creds[name]['password']
+        else:
+            smtp_server = input('Enter SMTP server: ')
+            smtp_port = int(input('Enter SMTP port: '))
+            username = input('Enter username (all text before the @ symbol): ')
+            password = getpass('Enter password: ')
+        self.client_utils = ClientUtils(smtp_server=smtp_server, smtp_port=smtp_port, username=username, password=password)
+        self.client_utils.connect()
 
-# Parse the arguments
-args = parser.parse_args()
+    def do_send(self, arg):
+        'Send an email and be prompted for the to, subject, and body. Usage: send'
+        to = input('To: ')
+        subject = input('Subject: ')
+        body = input('Body: ')
+        self.client_utils.send_email(to, subject, body)
+        print('Email sent successfully.')
 
-if(args.username == None):
-    args.username = input("Username: ")
-if(args.password == None):
-    args.password = input("Password: ")
-if(args.smtp_server == None):
-    args.smtp_server = input("SMTP Server: ")
-if(args.smtp_port == None):
-    args.smtp_port = input("SMTP Port: ")
-if(args.to != None):
-    args.to = args.to.split(",")
-    utils = ClientUtils(args.smtp_server, args.smtp_port, args.username, args.password)
-    utils.connect()
-    for recipient in args.to:
-        utils.send_email(recipient, args.subject, args.body)
+    def do_list(self, arg):
+        'List all emails. Usage: list'
+        emails = self.client_utils.receive_emails()
+        for email in emails:
+            print(f'{email["id"]}: {email["subject"]} ({email["from"]})')
 
-# Execute the command
-if args.command == 'login':
-    login(args.username, args.password)
-elif args.command == 'send':
-    send_email(args.username, args.password, args.to, args.subject, args.body)
-elif args.command == 'list':
-    list_emails(args.username, args.password)
-else:
-    print('Invalid command')
+    def do_quit(self, arg):
+        'Exit the shell. Usage: quit'
+        print('Goodbye!')
+        return True
+
+if __name__ == '__main__':
+    ClientShell().cmdloop()
